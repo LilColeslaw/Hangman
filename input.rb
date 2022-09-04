9c2a43bb0bf51a17de317df4dcda17de89c07849
Winslow Solomon <winsolo11@gmail.com>
2022-09-04 17:45:27 -0400

    Change wording

--- input.rb
+++ input.rb
@@ -3,12 +3,12 @@
 # will handle all user-input for the game
 class Input
   def self.guess(guessed)
-    puts 'Enter a new letter:'
+    puts 'Enter a letter:'
     letter = gets.chomp.downcase
     # make sure the letter hasn't been guessed, is only one letter, and is a letter (not a number)
     return letter unless guessed.include?(letter) || letter.length != 1 || letter.match?(/[^a-z]/)
 
-    puts 'Not a valid letter. '
+    puts 'Not a valid letter. Type in a single letter that you have not guessed before.'
     guess(guessed)
   end
 end
